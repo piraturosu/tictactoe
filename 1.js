a89caf13aaf9ed92b390ec892630e9b3fae7a904
George C <george.constantin33@gmail.com>
2024-02-26 12:23:17 +0000

    first

---
+++ 1.js
@@ -0,0 +1,12 @@
+let CANVAS = [null, null, null, null, null, null, null, null, null];
+let PLAYERS = { playerOne: 0, playerTwo: 1 };
+let CHOICE = { 0: 'X', 1: 'O' };
+console.log(CANVAS.length);
+
+function storeClickedId(clickedId) {
+  let choice = { playerOne: clickedId };
+}
+
+function insertPlayerChoice(playerOneChoice, playerTwoChoice) {}
+
+function gameCompletion() {}
