201306424adffde435d6c811a26b6a194079eaf1
George C <george.constantin33@gmail.com>
2024-03-07 17:30:28 +0000

    edit1

--- view.js
+++ view.js
@@ -1,7 +1,8 @@
 const message = document.getElementById('message');
-const scoreboardP1 = document.getElementById('scoreboardP1');
-const scoreboardP2 = document.getElementById('scoreboardP2');
 const boardView = document.getElementById('boardView');
+const scoreboards = document.getElementsByClassName('scoreboard');
+const scoreboardP1 = scoreboards[0];
+const scoreboardP2 = scoreboards[1];
 scoreboardP1.innerText = `Player 1: ${SCORE.PLAYER1}`;
 scoreboardP2.innerText = `Player 2: ${SCORE.PLAYER2}`;
 
@@ -10,14 +11,16 @@ let timer;
 function play(id) {
   const result = ticTacToe(id);
   if (result == null) return;
-  if (timer) {
-    clearTimeout(timer);
-  }
+
+  if (timer) clearTimeout(timer);
+
   storeScore(result);
   showResults(result);
-  timer = setTimeout(deleteMessage, 5000);
+
   resetState();
   resetBoardView();
+
+  timer = setTimeout(deleteMessage, 5000);
 }
 
 function showResults(result) {
